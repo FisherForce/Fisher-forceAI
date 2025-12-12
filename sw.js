e8cc046540f610561f05216abf56d88d84cacde4
FisherForce <thaolekana9@gmail.com>
2025-12-12 17:12:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,9 @@
-const CACHE_NAME = 'fisherforce-v1.0';
+const CACHE_NAME = 'fisherforce-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/app.js',
-  '/style.css', // si tu as un CSS séparé
-  '/logo.jpg',
-  '/resultat.html',
-  '/script.js',
-  '/manifest.json',
-  '/Capture d\'écran 2024-11-15 18380.png',
-  '/learn.js',
-  '/learnedPatterns.json',
-  '/openai.js',
-  '/package.json',
-  '/save-spot.php',
-  '/server.js',
-  '/sessions.json',
-  '/spots.json',
-  '/suggestLures.js',
-  '/privacy.html',
-  '/logo.jpg',
-  // Ajoute ici tes autres fichiers importants (images, sons, etc.)
+  '/logo.jpg'
 ];
 
 self.addEventListener('install', event => {
